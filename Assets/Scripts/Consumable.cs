da77069f429595fa269fce8f505c0e5434cc7ae6
Nikita Demkin <nikitademkin@gmail.com>
2025-11-15 23:17:45 +0300

    setup consumables on map

--- Assets/Scripts/Consumable.cs
+++ Assets/Scripts/Consumable.cs
@@ -6,9 +6,25 @@ public class Consumable : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 50.0f;
 
+    [SerializeField]
+    private GameObject effect;
+
     // Update is called once per frame
     void Update()
     {
        transform.Rotate(0.0f, 50.0f * Time.deltaTime, 0.0f, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (effect != null)
+            {
+                Destroy(effect);
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
